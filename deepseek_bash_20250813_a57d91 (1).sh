7187eca7be6e72a996645eddd55294d07ae4ebea
abhishekagrahari307-max <abhishekagrahari307@gmail.com>
2025-08-13 10:57:16 +0530

    Add files via upload

---
+++ deepseek_bash_20250813_a57d91 (1).sh
@@ -0,0 +1,7 @@
+# Create a new repo locally
+mkdir BharatOS-AbhishekAgrahari
+cd BharatOS-AbhishekAgrahari
+git init
+echo "# BharatOS - Abhishek Agrahari Edition" >> README.md
+git add README.md
+git commit -m "Initial commit"
